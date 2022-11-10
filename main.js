36eb58e4284073b426f3fb0b391fa8744fa82554
서석희 <seoki@seoki-4.local>
2022-11-11 01:38:52 +0900

    undefinded

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var cookie = require('cookie');
-var template = require('./template.js');
-var path = require('path');
-var mysql = require('mysql');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+const cookie = require('cookie');
+const mysql = require('mysql');
+const template = require('./template.js');
+const bodyParser = require('body-parser');
+const express = require('express');
+const api = express();
+
+
+api.use(express.json());
+api.use(bodyParser.json());
+api.use(express.urlencoded({extended:true}));
 
 var db = mysql.createConnection({
   host : 'localhost',
@@ -16,23 +23,22 @@ var db = mysql.createConnection({
 // database 버전관리
 
 db.connect(function(err){
-  if(err){
-    console.log("error occur!!\n");
-    throw err;
-  }
-});
+  if(err) throw err;
+  console.log("db connected\n");
+})
 
 
-function signin(req,res){
-    console.log(req.input_id);
-    var sql = `INSERT INTO login (ID,PWD,NICKNAME,CREATED) VALUES('${req.id}','${req.pwd}','${req.name}',NOW())`;
-    db.query(sql,function(err,result){
-      if(err) throw err;
-      console.log("successfuly to signin");
-      console.log(result);
-    });
-}
+// function signin(id,pwd,name){
+//     var sql = `INSERT INTO login_app(ID,PWD,NICKNAME,CREATED) VALUES('${id}','${pwd}','${name}',NOW())`;
+//     db.query(sql,function(err,result){
+//       console.log("successfuly to signin");
+//       console.log(result);
+//     });
+// }
 //회원가입 function 설정
+function login(id,pwd){
+  db.query("SELECT ID FROM login_app",)
+}
 
 
 function authIsOwner(req,res){
@@ -52,9 +58,14 @@ function authIsOwner(req,res){
 
 var app = http.createServer(function(req,res){
   var _url = req.url;
+  var qureyData = url.parse(_url,true).query;
   var pathname = url.parse(_url, true).pathname;
+
   
   if(pathname === '/'){
+    if(qureyData.id === undefined){
+
+    }
     var title = "Main";
     var body  = `
     <form action = "login_process" method = "post">
@@ -62,7 +73,8 @@ var app = http.createServer(function(req,res){
     <p><input type = "text" placeholder = "Enter ID" name = "ID"></p>
     <p><input type = "password" placeholder="Enter Password" name = "pwd"></p>
     <p><button type = "submit">Login</button></p>
-    <a href = "/signin"> 회원가입</a>
+    <p><a href = "/signin"> 회원가입</a></p>
+    <a href = "/creat"> 글 쓰기</a>
     </form>`
 
     var html = template.HTML(title,body);
@@ -81,19 +93,11 @@ var app = http.createServer(function(req,res){
       console.log(post);
       console.log(post.undefinedID, post.pwd);
       
-      // if(post.undefinedID =="seoki" && post.pwd == "1111"){
-      //   res.writeHead(302,{
-      //     "Set-Cookie" :[
-      //       `id = ${post.undefinedID}`,
-      //       `pwd = ${post.pwd}`]
-      //     });
-      //     auth = authIsOwner(req,res);
-      //   }
-
       auth = authIsOwner(req,res);
       if(auth){  //success to login
         title = "SUCCESS";
-        body = `<h3>Wellcome</h3>
+        body = 
+        `<h3>Wellcome</h3>
         <form action = "/logout_process" method = "post">
         <button type = "submit">logout</button>
         </form>`;
@@ -105,7 +109,7 @@ var app = http.createServer(function(req,res){
         res.end("fail");
       }
     });
-      
+
     }
     else if(pathname === "/logout_process"){
       console.log("try to log out");
@@ -124,14 +128,15 @@ var app = http.createServer(function(req,res){
         res.end();
       }); 
     }
+
     else if(pathname == "/signin"){
       var title = "SIGN IN Wellcome";
       var body = 
       `<h3>Wellcome</h3>
       <form action = "/signin_process" method = "post">
-      <p><input type = "text" placeholder = "UserID" name = "input_id"></p>
-      <p><input type = "text" placeholder = "UserPASSWORD" name = "input_pwd"></p>
-      <p><input type = "text" placeholder = "Username" name = "input_nickname"></p>
+      <p><input type = "text" placeholder = "UserID" name = "id"></p>
+      <p><input type = "text" placeholder = "UserPASSWORD" name = "pwd"></p>
+      <p><input type = "text" placeholder = "Username" name = "nickname"></p>
       <button type = "submit">회원가입</button>
       </form>`;
       var html = template.HTML(title,body);
@@ -141,23 +146,53 @@ var app = http.createServer(function(req,res){
     
     else if(pathname === "/signin_process"){
       var post = qs.parse(body);
-      
       req.on('data',function(data){
         body += data;
       });
       req.on('end',function(){
         var post = qs.parse(body);
-        console.log(post); 
+        console.log(post);
+        signin(post.undefinedid,post.pwd,post.nickname);
       });
-
-      res.writeHead(302,{Location : '/'});
+      res.writeHead(302,
+        {Location : '/'});
       res.end();
   }
-    else {
-      res.writeHead(404);
-      res.end('Not found');
-    }
+  else if(pathname === "/creat"){
+    fs.readdir("./data",function(){
+      var tilte = "글 생성";
+      var body = `
+      <form action = "/creat_process" method = "get">
+      <p><input type = "text" name = "title" placeholder = "글 제목"></p>
+      <p><textarea name = "des" placeholder = "글 내용"></textarea></p>
+      <p><input type = "submit"></p>
+      </form>
+      `
+      var html = template.HTML(tilte,body);
+      res.writeHead(200);
+      res.end(html);
+    });
+  }
+  else if(pathname === '/creat_process'){
+    var body = '';
+    req.on('data',function(data){ //data를 수신할때마다 callback을 호출하라
+      body += data;
+    });
+    req.on('end',function(){ 
+      var post = qs.parse(body);
+      var tilte = post.tilte;
+      var desc = post.desc;
+
+      console.log(tilte,desc);
+    });
+    
+  }
+  else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 });
+
 
 
 app.listen(3000,function(){
